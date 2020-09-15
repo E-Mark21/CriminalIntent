ddf62979f7654c54635131f1a47d6d5c9074e41a
E-Mark21 <yevheniimarkosh@gmail.com>
2020-09-15 11:25:21 +0300

    initial commit

--- app/src/main/java/com/bignerdbranch/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdbranch/criminalintent/Crime.java
@@ -37,9 +37,26 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime() {
-        mId = UUID.randomUUID();
+        this(UUID.randomUUID());
+    }
+
+    public Crime(UUID id) {
+        mId = id;
         mDate = new Date();
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
+
+    public String getPhotoFilename() {
+        return "IMG_" + getId().toString() + ".jpg";
     }
 }
